57d0ddb42b2c5ef05a3cf3f637af0599d2b3f3bb
Tisdale Fry <tisdalefry@gmail.com>
2020-03-05 18:24:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   arr_str = string.split(' ')
   rev_str = []
-  arr_str.collect { |word| word.reverse}
-  arr_str.join(' ')
+  arr_str.collect { |word| rev_str << word.reverse}
+  rev_str.join(' ')
 end
